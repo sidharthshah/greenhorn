ee314cbeae59c14bdfb024510a43012292ecef81
Sidharth Shah <sidharth@fafadiatech.com>
2014-06-10 22:59:53 +0530

    first rails app commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Greenhorn::Application.config.session_store :cookie_store, key: '_greenhorn_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Greenhorn::Application.config.session_store :active_record_store
